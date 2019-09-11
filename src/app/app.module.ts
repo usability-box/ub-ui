4bacc444d63bb127952b3c65a177b63ea80fe339
guilherme.ferreira <guilherme.ferreira>
2019-09-11 19:21:46 -0300

    Add styles, fontawesome and create login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    LoginModule,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SigninComponent } from './signin/signin.component';
+
+
+
+@NgModule({
+  declarations: [
+    SigninComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SigninComponent
+  ]
+})
+export class LoginModule { }
